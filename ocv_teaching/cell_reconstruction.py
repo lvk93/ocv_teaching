58b00a3b5522fe5853ceac077afc03a5855d1a3f
LvK <lvk@hotmail.de>
2025-07-09 11:10:53 +0200

    Refactored cell_reconstruction

--- ocv_teaching/cell_reconstruction.py
+++ ocv_teaching/cell_reconstruction.py
@@ -13,23 +13,20 @@ class CellOCVReconstruction:
         self.cath = cath_material
         self.an = an_material
 
-    def reconstruct(self, np_ratio:float, v_min:float, v_max:float) -> (float, float, float, float):
+    def get_stoichiometries(self, np_ratio:float, v_min:float, v_max:float) -> (float, float, float, float):
 
         np_offset = np_ratio*self.an.formation_loss - self.cath.formation_loss
 
         sol_an_fun = lambda x: (-x+1-np_offset)/ np_ratio
-        
-        soc_vec = np.linspace(0, 1, 100)
-        volt_cath_cha = interpolate(self.cath.ocv.soc, self.cath.ocv.get_voltage('charge'), soc_vec)
-        volt_cath_dis = interpolate(self.cath.ocv.soc, self.cath.ocv.get_voltage('discharge'), soc_vec)
-        volt_an_cha = interpolate(self.an.ocv.soc, self.an.ocv.get_voltage('charge'), sol_an_fun(soc_vec))
-        volt_an_dis = interpolate(self.an.ocv.soc, self.an.ocv.get_voltage('discharge'), sol_an_fun(soc_vec))
+        an0 = sol_an_fun(0)
+        an1 = sol_an_fun(1)
 
         # Determine cell voltage
-        volt_cell_cha = volt_cath_cha - volt_an_cha
-        volt_cell_dis = volt_cath_dis - volt_an_dis
+        volt_cell_cha = self.reconstruct_voltage(an0,0,an1,1,direction="charge")[0]
+        volt_cell_dis = self.reconstruct_voltage(an0,0,an1,1,direction="discharge")[0]
 
         # Determine stoichiometries
+        soc_vec = np.linspace(0, 1, 100)
         an0 = interpolate(volt_cell_dis,sol_an_fun(soc_vec), v_min)
         an1 = interpolate(volt_cell_cha,sol_an_fun(soc_vec), v_max)
         cath0 = interpolate(volt_cell_dis,soc_vec, v_min)
@@ -38,14 +35,23 @@ class CellOCVReconstruction:
 
     def simulate_aging_modes(self,LAMPE,LAMNE,LLI):
         return
+    
+    def reconstruct_voltage(self,an0:float, cath0:float, an1:float, cath1:float,direction="charge") -> (np.ndarray, np.ndarray, np.ndarray):
+        """
+        Reconstructs the full cell OCV based on the anode and cathode materials.
+        """
+        soc_vec = np.linspace(0, 1, 100)
+        soc_an = (an1-an0) * soc_vec + an0
+        soc_cath = (cath1-cath0) * soc_vec + cath0
+        
+        volt_cath = interpolate(self.cath.ocv.soc, self.cath.ocv.get_voltage(direction), soc_cath)
+        volt_an = interpolate(self.an.ocv.soc, self.an.ocv.get_voltage(direction), soc_an)
 
-    def plot(self):
-        soc, v_cell = self.reconstruct()
-        fig, ax = new_plot()
-        ax.plot(soc, v_cell)
-        ax.set_xlabel('Full-Cell SOC')
-        ax.set_ylabel('Cell Voltage (V)')
-        ax.set_title(f'Full-Cell OCV Reconstruction')
-        plt.show()
+        volt_cell = volt_cath - volt_an
+
+        return volt_cell, volt_cath, volt_an
+
+
+
 
 
