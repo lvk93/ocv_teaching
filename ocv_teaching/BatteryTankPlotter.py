ab9ffec63698c4882797f0d17daae77eda36f6f5
LvK <lvk@hotmail.de>
2025-07-19 01:01:12 +0200

    Separated classes

--- ocv_teaching/BatteryTankPlotter.py
+++ ocv_teaching/BatteryTankPlotter.py
@@ -1,170 +1,126 @@
-import numpy as np
+from .cell_reconstruction import CellOCVReconstruction
 import matplotlib.pyplot as plt
-import pybamm
-
-
+from utils import interpolate
 class BatteryTankPlotter:
-    def __init__(
-        self,
-        ocv_anode,
-        ocv_cathode,
-        soc_an_fun=lambda soc: soc,
-        soc_ca_fun=lambda soc: 1 - soc,
-        np_ratio=1.0,
-        v_min=None,
-        v_max=None,
-        lamne=0.0,
-        lampe=0.0,
-        resolution=500,
-    ):
-        self.ocv_anode = ocv_anode
-        self.ocv_cathode = ocv_cathode
-        self.soc_an_fun = soc_an_fun
-        self.soc_ca_fun = soc_ca_fun
-        self.np_ratio = np_ratio
-        self.v_min = v_min
-        self.v_max = v_max
-        self.lamne = lamne
-        self.lampe = lampe
+    """
+    Uses CellOCVReconstruction to plot dQ/dV 'tanks' and OCV curves for BOL and aged cells.
+    """
+    def __init__(self, reconstructor: CellOCVReconstruction, resolution=500):
+        self.rec = reconstructor
         self.resolution = resolution
-
-        self.colors = {
-            "gray": "#333333",
-            "bright_gray": "#E7E6E6",
-            "gray_blue": "#44546A",
-            "navy_blue": "#003B73",
-            "light_blue": "#5DADE2",
-            "orange": "#F39C12",
-            "green": "#70AD47",
+        self.bc = {
+            'an_bol': '#333333',
+            'ca_bol': '#5DADE2',
+            'an_aged': '#444444',
+            'ca_aged': '#003B73',
+            'fill_bol': '#44546A',
+            'fill_aged': '#70AD47',
+            'delta': '#F39C12'
         }
 
-    def _compute_ocv_and_dqdv(self, ocv_func):
-        soc_grid = np.linspace(0, 1, self.resolution)
-        voltage = ocv_func(soc_grid)
-        dVdQ = np.gradient(voltage, soc_grid)
-        dQdV = 1.0 / dVdQ
-        return soc_grid, voltage, dQdV
+    def _compute_electrode_curves(self, an0, cath0, an1, cath1, direction):
+        soc = np.linspace(1,0, self.resolution)
+        V_cell, V_ca, V_an, sol_an, sol_ca = self.rec.reconstruct_voltage(
+            an0, cath0, an1, cath1, direction, soc)
+        dQdV_an = 1 / np.gradient(V_an, sol_an)
+        dQdV_ca = 1 / np.gradient(V_ca, sol_ca)
+        return soc, sol_an, V_an, dQdV_an, sol_ca, V_ca, dQdV_ca, V_cell
 
-    def _plot_voltage_limit_lines(self, ax, soc_grid):
-        lines = []
-
-        if self.v_min is not None:
-            # anode at SOC=1 (fully lithiated), cathode at SOC=1 (fully lithiated)
-            V_an1 = self.ocv_anode(self.soc_an_fun(1))
-            V_ca0 = self.ocv_cathode(self.soc_ca_fun(1))
-            lines.append(("an1", -V_an1))
-            lines.append(("cath0", -V_ca0))
-
-        if self.v_max is not None:
-            # anode at SOC=0 (delithiated), cathode at SOC=0 (delithiated)
-            V_an0 = self.ocv_anode(self.soc_an_fun(0))
-            V_ca1 = self.ocv_cathode(self.soc_ca_fun(0))
-            lines.append(("an0", -V_an0))
-            lines.append(("cath1", -V_ca1))
-
-        for label, y in lines:
-            ax.axhline(y=y, color=self.colors["green"], linestyle=":", linewidth=1)
-            ax.text(
-                x=4.6, y=y, s=label, va="center", ha="right", fontsize=9, color=self.colors["green"]
-            )
-
-    def _plot_dqdv_tanks(self, ax, soc, soc_grid, V_an, dQdV_an, V_ca, dQdV_ca):
-        soc_an = np.clip(soc_grid, 0.0, soc)
-        soc_ca = np.clip(soc_grid, 0.0, 1 - soc)
-
-        V_an_cur = self.ocv_anode(soc_an)
-        V_ca_cur = self.ocv_cathode(soc_ca)
-
-        dQdV_an_cur = np.interp(soc_an, soc_grid, dQdV_an) * (1 - self.lamne) * self.np_ratio
-        dQdV_ca_cur = np.interp(soc_ca, soc_grid, dQdV_ca) * (1 - self.lampe)
-
-        # Plot tanks
-        for sign in [1, -1]:
-            ax.plot(sign * dQdV_an * (1 - self.lamne) * self.np_ratio, -V_an,
-                    color=self.colors["gray"], label="Graphite tank" if sign == 1 else None)
-            ax.plot(sign * dQdV_ca * (1 - self.lampe), -V_ca,
-                    color=self.colors["light_blue"], label="NCM tank" if sign == 1 else None)
-
-            ax.plot(sign * dQdV_an, -V_an * self.np_ratio,
-                    color=self.colors["gray"], linestyle='--')
-            ax.plot(sign * dQdV_ca, -V_ca,
-                    color=self.colors["light_blue"], linestyle='--')
-
-        # Fill tanks
-        for dQdV, V in [(dQdV_an_cur, V_an_cur), (dQdV_ca_cur, V_ca_cur)]:
-            for sign in [1, -1]:
-                ax.fill_betweenx(-V, sign * dQdV, 0,
-                                 color=self.colors["navy_blue"], alpha=0.8)
-
-        # Voltage difference arrow
-        delta_V = V_ca_cur.min() - V_an_cur.min()
-        mid_y = -0.5 * (V_ca_cur.min() + V_an_cur.min())
-        ax.annotate('', xy=(0, -V_ca_cur.min()), xytext=(0, -V_an_cur.min()),
-                    arrowprops=dict(arrowstyle='<->', color=self.colors["orange"], linewidth=2))
-        ax.text(0.1, mid_y, f'ΔV = {delta_V:.3f} V',
-                color=self.colors["orange"], va='center')
-
-        # Add voltage limit lines if set
-        self._plot_voltage_limit_lines(ax, soc_grid)
-
-        # Axes formatting
-        ax.set_xlabel("dQ/dV [Ah/V]")
-        ax.set_ylabel("Voltage [V]")
-        ax.set_xlim(-5, 5)
-        ax.set_ylim(-5, 0)
-        yticks = np.arange(-5, 1, 1)
-        ax.set_yticks(yticks)
-        ax.set_yticklabels([abs(int(t)) for t in yticks])
-        ax.legend(loc="center left")
-        ax.grid(True)
-
-    def _plot_ocv_curve(self, ax, soc, soc_grid):
-        soc_an = self.soc_an_fun(soc_grid)
-        soc_ca = self.soc_ca_fun(soc_grid)
-
-        V_an = self.ocv_anode(soc_an)
-        V_ca = self.ocv_cathode(soc_ca)
-        V_cell = V_ca - V_an
-
-        delta_V = self.ocv_cathode(self.soc_ca_fun(soc)) - self.ocv_anode(self.soc_an_fun(soc))
-
-        if self.v_min is not None and self.v_max is not None and self.v_max > self.v_min:
-            # Interpolate SoC values corresponding to Vmin and Vmax
-            soc_vs_voltage = lambda V_target: np.interp(V_target, V_cell, soc_grid)
-
-            soc_vmin = soc_vs_voltage(self.v_min)
-            soc_vmax = soc_vs_voltage(self.v_max)
-
-            if soc_vmax == soc_vmin:  # Avoid divide by zero
-                soc_norm = soc_grid * 0
-                soc_point = 0
-            else:
-                soc_norm = (soc_grid - soc_vmin) / (soc_vmax - soc_vmin)
-                soc_point = (soc - soc_vmin) / (soc_vmax - soc_vmin)
-
-            ax.set_xlim(0, 1)
-            ax.set_xlabel("Normalized SoC (0 at Vmin, 1 at Vmax)")
-        else:
-            soc_norm = soc_grid
-            soc_point = soc
-            ax.set_xlim(0, 1)
-            ax.set_xlabel("State of Charge (SOC)")
-
-        ax.plot(soc_norm, V_cell, color=self.colors["navy_blue"], label="Cell OCV")
-        ax.plot(soc_point, delta_V, color=self.colors["navy_blue"], marker='o', markersize=5, label="Current SOC")
-        ax.set_ylim(0, 5)
-        ax.set_ylabel("Voltage [V]")
-        ax.grid(True)
-        ax.legend()
-
-
-    def plot(self, soc=0.5):
-        soc_grid, V_an, dQdV_an = self._compute_ocv_and_dqdv(self.ocv_anode)
-        _, V_ca, dQdV_ca = self._compute_ocv_and_dqdv(self.ocv_cathode)
-
+    def plot(self, soc=0.5, lampe=0.0, lamne=0.0, lli=0.0):
+        ## Full curves
+        soc_vec = np.linspace(0,1,self.resolution)
+        V_an = self.rec.an(soc_vec)
+        V_cath = self.rec.cath(soc_vec)
+        dQdV_an = 1 / np.gradient(V_an, soc_vec)
+        dQdV_ca = 1 / np.gradient(V_cath, soc_vec)
+        
+        # plotting
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-        self._plot_dqdv_tanks(ax1, soc, soc_grid, V_an, dQdV_an, V_ca, dQdV_ca)
-        self._plot_ocv_curve(ax2, soc, soc_grid)
 
+        ## Plot the full curves BOL
+        bc = self.bc
+        ax1.plot(dQdV_an*self.rec.np_ratio, -V_an, color=bc['an_aged'],linestyle='--')
+        ax1.plot(-dQdV_an*self.rec.np_ratio, -V_an, color=bc['an_aged'],linestyle='--')
+        ax1.plot(dQdV_ca, -V_cath, color=bc['ca_bol'],linestyle='--')
+        ax1.plot(-dQdV_ca, -V_cath, color=bc['ca_bol'],linestyle='--')
+
+        ## Plot the full curves aged
+        ax1.plot(dQdV_an*self.rec.np_ratio*(1-lamne), -V_an, color=bc['an_aged'], label='Anode')
+        ax1.plot(-dQdV_an*self.rec.np_ratio*(1-lamne), -V_an, color=bc['an_aged'])
+        ax1.plot(dQdV_ca*(1-lampe), -V_cath, color=bc['ca_bol'], label='Cathode')
+        ax1.plot(-dQdV_ca*(1-lampe), -V_cath, color=bc['ca_bol'])
+
+        # Compute aged filling
+        # BOL stoichiometries
+        an0_bol,cath0_bol,an1_bol,cath1_bol = self.rec.get_stoichiometries()
+        # aged stoichiometries
+        an0_a, ca0_a, an1_a, ca1_a = self.rec.simulate_aging_modes(lampe, lamne, lli)
+        # get aged voltages
+        v_an0 = self.rec.an(an0_a)
+        v_an1 = self.rec.an(an1_a)
+        v_ca1 = self.rec.cath(ca1_a)
+        v_ca0 = self.rec.cath(ca0_a)
+        # curves
+        ax1.axhline(y=-v_an0, color=self.bc["fill_aged"], linestyle=":", linewidth=1)
+        ax1.axhline(y=-v_an1, color=self.bc["fill_aged"], linestyle=":", linewidth=1)
+        ax1.axhline(y=-v_ca0, color=self.bc["fill_aged"], linestyle=":", linewidth=1)
+        ax1.axhline(y=-v_ca1, color=self.bc["fill_aged"], linestyle=":", linewidth=1)
+
+        # Reconstruct cell voltage
+        V_cell, V_ca, V_an, sol_an, sol_ca = self.rec.reconstruct_voltage(
+            an0_a, ca0_a, an1_a, ca1_a, "charge",soc_vec)
+        
+        ax2.plot(soc_vec,V_cell)
+
+        # (soc_full, sa_b, Va_b, dQa_b, sc_b, Vc_b, dQc_b, Vcell_b) = self._compute_electrode_curves(
+        #     0, 1, 1, 0, 'charge')
+
+        
+
+        # aged tanks fill
+        # mask_a = soc_full <= soc
+        # Determine voltage for soc
+        sol_an_cur = an0_a+(an1_a-an0_a)*(soc)
+        sol_ca_cur = ca0_a+(ca1_a-ca0_a)*(soc)
+        sol_an_vec = np.linspace(0,sol_an_cur,100)
+        sol_ca_vec = np.linspace(0,sol_ca_cur,100)
+        V_an_cur = self.rec.an(np.linspace(0,sol_an_cur,100))
+        V_ca_cur = self.rec.cath(np.linspace(0,sol_ca_cur,100))
+        dQ_an_cur = interpolate(soc_vec,dQdV_an,sol_an_vec)
+        dQ_cath_cur = interpolate(soc_vec,dQdV_ca,sol_ca_vec)
+        ax1.fill_betweenx(-V_an_cur, -dQ_an_cur*self.rec.np_ratio*(1-lamne), dQ_an_cur*self.rec.np_ratio*(1-lamne), color=bc['ca_aged'], alpha=0.5)
+        ax1.fill_betweenx(-V_ca_cur, -dQ_cath_cur*(1-lampe), dQ_cath_cur*(1-lampe), color=bc['ca_aged'], alpha=0.5)
+        
+        # delta V arrow
+        dV = -V_an_cur[-1].min() + V_ca_cur[-1].max()
+        ax1.annotate('', xy=(0, -V_an_cur[-1].min()), xytext=(0,-V_ca_cur[-1].max()),
+                     arrowprops=dict(arrowstyle='<->', color=bc['delta'], lw=2))
+        ymid = -0.5*(V_an_cur[-1].min() + V_ca_cur[-1].max())
+        ax1.text(0.1, ymid, f'ΔV={dV:.3f} V', color=bc['delta'])
+
+        # Labels etc.
+        ax1.set_xlabel('dQ/dV [Ah/V]')
+        ax1.set_ylabel('Voltage [V]')
+        ax1.set_xlim(-5, 5)
+        ax1.set_ylim(-5, 0)
+        yticks = np.arange(-5, 1, 1)
+        ax1.set_yticks(yticks)
+        ax1.legend(loc='center left')
+        ax1.grid(True)
+        #Get BOL and aged OCV curves         
+        (soc_full, sa_b, Va_b, dQa_b, sc_b, Vc_b, dQc_b, Vcell_b) = self._compute_electrode_curves(
+           an0_bol, cath0_bol, an1_bol, cath1_bol, 'charge')
+        (soc_full, sa_a, Va_a, dQa_a, sc_a, Vc_a, dQc_a, Vcell_a) = self._compute_electrode_curves(
+            an0_a, ca0_a, an1_a, ca1_a, 'charge')
+        # Plot OCV curve
+        ax2.plot(soc_full, Vcell_b, color=bc['ca_aged'], label='Cell BOL', linestyle="--")
+        ax2.plot(soc_full, Vcell_a, color=bc['ca_aged'], )
+        # Plot Current SOC point
+        ax2.plot(soc,dV,color=bc["ca_aged"],marker="o",markersize=5)
+        ax2.set_xlabel('SOC')
+        ax2.set_ylabel('Voltage [V]')
+        ax2.legend()
+        ax2.grid(True)
+        ax2.set_xlim(0,1)
         plt.tight_layout()
         plt.show()
--- ocv_teaching/cell_reconstruction.py
+++ ocv_teaching/cell_reconstruction.py
@@ -1,73 +1,85 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
-
-
-def interpolate(x, y, x_new):
-    """1D linear interpolation"""
-    return np.interp(x_new, x, y)
+from utils import interpolate
 
 
 class CellOCVReconstruction:
     """
     Reconstructs full-cell OCV from two PyBaMM OCV functions and their N:P ratio.
+    Provides methods to compute stoichiometries and voltage curves.
     """
-    def __init__(self, cath_ocv_func, an_ocv_func, np_ratio: float, v_min=2.5, v_max=4.2):
-        self.cath = cath_ocv_func  # PyBaMM OCV function: soc → V
-        self.an = an_ocv_func      # PyBaMM OCV function: soc → V
+    def __init__(self, cath_ocv_func, an_ocv_func, np_ratio: float,
+                 v_min=2.5, v_max=4.2, formation_loss_an=0.0, formation_loss_ca=0.0):
+        self.cath = cath_ocv_func    # PyBaMM OCV: soc -> V
+        self.an = an_ocv_func        # PyBaMM OCV: soc -> V
         self.np_ratio = np_ratio
-        self.np_offset = np_ratio * 0.0 - 0.0  # Assume no formation loss by default
+        # formation losses (fraction) can be provided
+        self.np_offset = np_ratio * formation_loss_an - formation_loss_ca
         self.v_min = v_min
         self.v_max = v_max
 
     def align_anode_cathode(self, sol_cath, np_ratio=None, np_offset=None):
+        """
+        Compute anode stoichiometry from cathode stoichiometry based on N:P ratio and offset.
+        """
         if np_ratio is None:
             np_ratio = self.np_ratio
         if np_offset is None:
             np_offset = self.np_offset
         return (-sol_cath + 1 - np_offset) / np_ratio
 
-    def reconstruct_voltage(self, an0, cath0, an1, cath1, direction="charge",
-                            soc_vec=np.linspace(0, 1, 100)):
-        soc_an = (an1 - an0) * soc_vec + an0
-        soc_cath = (cath1 - cath0) * soc_vec + cath0
-
-        volt_cath = self.cath(soc_cath)
-        volt_an = self.an(soc_an)
-        volt_cell = volt_cath - volt_an
-
-        return volt_cell, volt_cath, volt_an
+    def reconstruct_voltage(self, an0, cath0, an1, cath1,
+                            direction='charge',
+                            soc_vec=None):
+        """
+        Reconstruct full-cell and half-cell voltages along a SOC vector.
+        Returns:
+          V_cell, V_cath, V_an, soc_an, soc_cath
+        """
+        if soc_vec is None:
+            soc_vec = np.linspace(0, 1, 100)
+        soc_an = an0 + (an1 - an0) * soc_vec
+        soc_cath = cath0 + (cath1 - cath0) * soc_vec
+        V_cath = self.cath(soc_cath)
+        V_an = self.an(soc_an)
+        V_cell = V_cath - V_an
+        return V_cell, V_cath, V_an, soc_an, soc_cath
 
     def get_stoichiometries(self, np_ratio=None, np_offset=None,
                             v_min=None, v_max=None):
+        """
+        Compute the anode/cathode stoichiometries at cell cutoff voltages.
+        Returns: an0, cath0, an1, cath1
+        """
         if v_min is None:
             v_min = self.v_min
         if v_max is None:
             v_max = self.v_max
-
         if np_ratio is None:
             np_ratio = self.np_ratio
         if np_offset is None:
             np_offset = self.np_offset
-
-        an0 = self.align_anode_cathode(0, np_ratio, np_offset)
-        an1 = self.align_anode_cathode(1, np_ratio, np_offset)
-
-        soc_vec = np.linspace(0, 1, 100)
-        an_grid = self.align_anode_cathode(soc_vec, np_ratio, np_offset)
-
-        # Compute full-cell voltage curves
-        volt_cell_cha = self.reconstruct_voltage(an0, 0, an1, 1, "charge")[0]
-        volt_cell_dis = self.reconstruct_voltage(an0, 0, an1, 1, "discharge")[0]
-
-        # Interpolate to find stoichiometries at voltage limits
-        an0 = interpolate(volt_cell_dis, an_grid, v_min)
-        an1 = interpolate(volt_cell_cha, an_grid, v_max)
-        cath0 = interpolate(volt_cell_dis, soc_vec, v_min)
-        cath1 = interpolate(volt_cell_cha, soc_vec, v_max)
-
-        return an0, cath0, an1, cath1
+        an0 = self.align_anode_cathode(1, np_ratio, np_offset)
+        an1 = self.align_anode_cathode(0, np_ratio, np_offset)
+        cath0 = 1.0
+        cath1 = 0.0
+        grid = np.linspace(0,1, 200)
+        vc_dis, _, _, _, _ = self.reconstruct_voltage(an0, cath0, an1, cath1, 'charge', grid)
+        vc_cha, _, _, _, _ = self.reconstruct_voltage(an0, cath0, an1, cath1, 'charge', grid)
+        soc0_cut = interpolate(vc_dis, grid, v_min)
+        soc1_cut = interpolate(vc_cha, grid, v_max)
+        an0_cut = soc0_cut*(an1-an0)+an0
+        an1_cut = soc1_cut*(an1-an0)+an0
+        cath0_cut = soc0_cut*(cath1-cath0)+cath0
+        cath1_cut = soc1_cut*(cath1-cath0)+cath0
+        return an0_cut, cath0_cut, an1_cut, cath1_cut
 
     def simulate_aging_modes(self, lampe, lamne, lli):
+        """
+        Compute aged stoichiometries given aging fractions.
+        Returns: an0, cath0, an1, cath1 for aged state.
+        """
         np_aged = self.np_ratio * (1 - lamne) / (1 - lampe)
         np_offset_aged = 1 - (1 - self.np_offset) * (1 - lli) / (1 - lampe)
         return self.get_stoichiometries(np_ratio=np_aged, np_offset=np_offset_aged)
